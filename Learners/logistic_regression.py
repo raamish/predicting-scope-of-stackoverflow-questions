843edb2a2899b7425cec2fce1e72ebd957b52bb5
ishaaann <ishanhanda1@gmail.com>
2017-04-12 19:35:28 +0530

    Computing Weights Using Logistic Regression Done

--- Learners/logistic_regression.py
+++ Learners/logistic_regression.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
 
 np.random.seed(12)
 num_observations = 100000
@@ -15,6 +18,29 @@ def log_likelihood(features, target, weights):
 	ll = np.sum(target*scores - np.log(1 + np.exp(scores)))
 	return ll
 
+def logistic_regression(features, target, num_steps, learning_rate, add_intercept):
+	if add_intercept:
+		intercept = np.ones((features.shape[0], 1))
+		features = np.hstack((intercept, features))
+
+	weights = np.zeros(features.shape[1])
+    
+	for step in xrange(num_steps):
+		scores = np.dot(features, weights)
+		predictions = sigmoid(scores)
+
+        # Update weights with gradient
+		output_error_signal = target - predictions
+		gradient = np.dot(features.T, output_error_signal)
+		weights += learning_rate * gradient
+        
+        # Print log-likelihood every so often
+		if step % 10000 == 0:
+			print "Congrats! You have completed " + str(step) + " steps"
+        
+	return weights
+
+
 def plot_scatter(dataset):
 	plt.figure(figsize=(12,8))
 	plt.scatter(dataset['ViewCount'],dataset['AutomatedReadingIndex'], c=colors)
@@ -26,24 +52,39 @@ def plot_scatter(dataset):
 
 def read_data():
 	dataset = pd.read_csv(combined_dataset_file, header=0)
-	return dataset
-
-
-if __name__ == '__main__':
-	dataset = read_data()
-	dataset.drop(['Body','Title','Tags','AcceptedAnswerId','CreationDate','ClosedDate','DeletionDate','Reason'], inplace = True, axis = 1)
-	print "Values containing NaN"
+	dataset.drop(['Body','Title','Tags','AcceptedAnswerId','CreationDate','ClosedDate','DeletionDate','Reason','Unnamed: 0'], inplace = True, axis = 1)
 	for column in dataset:
 		if dataset[column].isnull().any():
 			print column
 	dataset['FavoriteCount'].fillna(0.0, inplace = True)
-	plot_scatter(dataset)
-	dataset_arr = dataset.as_matrix()
-	k = 0
-	for i in dataset_arr:
-		if k == 5:
-			break
-		print i
-		print ""
-		print ""
-		k += 1
+	train, test = train_test_split(dataset, test_size = 0.2)
+	return train, test
+
+# Main function for Logistic Regression
+if __name__ == '__main__':
+	train, test = read_data()
+	print train.head(5)
+	print test.head(5)
+	y = train['OpenOrClosed']
+	y_test = test['OpenOrClosed']
+	train.drop(['OpenOrClosed'], inplace = True, axis = 1)
+	test.drop(['OpenOrClosed'], inplace = True, axis = 1)
+	y_answer = y.as_matrix()
+	y_answer_test = y_test.as_matrix()
+	plot_scatter(train)
+	x_answer = train.as_matrix()
+	x_answer_test = test.as_matrix()
+	
+	weights = weights = logistic_regression(x_answer, y_answer,
+                     num_steps = 30000, learning_rate = 5e-5, add_intercept=True)
+	print "first answer:"
+	print weights
+
+	#For Validation I am checking whether the weight computed by my code is same as what sklearn optimized version gives me.
+	clf = LogisticRegression(fit_intercept=True, C = 1e15)
+	clf.fit(x_answer, y_answer)
+
+	print "second answer:"
+	print clf.intercept_, clf.coef_
+
+	
